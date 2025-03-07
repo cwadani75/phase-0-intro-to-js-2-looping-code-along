f4670a133f45e96b690dbffd1d6d2bdb12e37bfc
cwadani75 <cwadani75@gmail.com>
2025-03-07 20:52:19 +0300

    loop labs

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Function to return an array of thank you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a given number to zero
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions so they can be tested
+  module.exports = { writeCards, countDown };
+  
